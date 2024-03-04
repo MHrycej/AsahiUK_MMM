0a9be7e0019136609a923921ca3cc851c8034d3b
Mattias Mutso <mattias.mutso@externi.cz>
2024-03-04 15:14:43 +0000

    exploration

--- nielsen_exploration.R
+++ nielsen_exploration.R
@@ -1,4 +1,4 @@
-
+library(writexl)
 library(readxl)
 library(here)
 library(ISOweek)
@@ -16,11 +16,11 @@ source(paste(directory_path, "functions/sellout_data_read_v01.R", sep = "/")) # 
 
 nielsen = nielsen.creation(
   "C:/Users/MHrycej/OneDrive - ABEG/Martin/Projects/MMM/R GIT/AsahiUK_MMM", 
-  "PNA_MULTIPLES_GLASS_330ML_10PACK",  # selection of model
+  "PNA_MULTIPLES_GLASS_330ML_4PACK",  # selection of model
   c("peroni"), # BRAND aggregation brand strings to search for
   c("peroni", "moretti", "madri", "estrella", "asd", "san miguel", "heineken", "cruzcampo", "corona", "stella","budweiser"), # SKU aggregation brand strings to limit our SKUs
   ""                 # SKU aggregation SKU strings to search for
-  
+
 )
 
 
@@ -62,3 +62,6 @@ result_transposed <- result %>%
   pivot_longer(cols = -year, names_to = "SKU", values_to = "Volume") %>%
   pivot_wider(names_from = year, values_from = Volume, values_fill = 0) %>%
   arrange(desc(`2023`)) 
+
+# Write result_transposed to an Excel file in the specified directory
+write_xlsx(result_transposed, path = file.path(directory_path, "result_transposed_v1.xlsx"))
